0ef4fb7947d4381aeb877184e353543a9f7be018
eugene <shapovalovei@gmail.com>
2017-02-08 20:11:43 +0200

    Add logging

--- src/main/java/utils/JiraHelper.java
+++ src/main/java/utils/JiraHelper.java
@@ -1,6 +1,8 @@
 package utils;
 
 import okhttp3.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -11,6 +13,7 @@ import java.io.StringWriter;
  * Created by vnaksimenko on 25.11.16.
  */
 public class JiraHelper {
+    //TODO: Change OAuth access tokens, url, project key
     private final static String AUTH_HEADER = "Z3JpbnZhbGQyMDE2QGdtYWlsLmNvbTpxMXExdzF3MQ==";
     private final static String JIRA_BASE_URL = "https://auto2020.atlassian.net/";
     private final static String JIRA_URL = JIRA_BASE_URL + "rest/api/2";
@@ -19,7 +22,12 @@ public class JiraHelper {
     private static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("application/json");
     private final static OkHttpClient client = new OkHttpClient();
 
+    private static Log log = LogFactory.getLog(JiraHelper.class);
+
     public static String publishJira(String title, String description) throws JSONException, IOException {
+
+        log.info("Create Jira Issues");
+
         JSONObject data = new JSONObject();
         JSONObject fields = new JSONObject();
         JSONObject project = new JSONObject();
@@ -47,7 +55,17 @@ public class JiraHelper {
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         JSONObject resp = new JSONObject(response.body().string());
-        return JIRA_BASE_URL + "browse/" + resp.getString("key");      // URL to created Jira ticket
+        String issuesLink = JIRA_BASE_URL + "browse/" + resp.getString("key");      // URL to created Jira ticket
+
+        String logString = "Project: " + project + "\n"
+                + "Title: " + title + "\n"
+                + "Issues Type: " + issueType + "\n"
+                + "Description: " + description + "\n"
+                + "Issues link: " + issuesLink;
+
+        CommonFunctions.log("Issues: " + resp.getString("key"), logString);
+
+
+        return issuesLink;
     }
 }
-
