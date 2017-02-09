e1b25b52bd0ba4163714e8f8a4654a581bce370d
eugene <shapovalovei@gmail.com>
2017-02-09 19:04:20 +0200

    add steps + page object

---
+++ src/main/java/pages/PayPalWidget.java
@@ -0,0 +1,58 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class PayPalWidget extends Component {
+
+    private By email = By.id("email");
+    private By password = By.id("password");
+    private By logInButton = By.name("btnLogin");
+    private By continuePayButton = By.id("confirmButtonTop");
+    private By iframe = By.name("injectedUl");
+    private By spinner = By.id("spinner");
+    private By preloaderSpinner = By.id("preloaderSpinner");
+
+    @Override
+    public boolean isPage() {
+        waitForElementVisible(this.iframe);
+        return true;
+    }
+
+    public PayPalWidget actionLogin() {
+        driver.findElement(this.logInButton).click();
+        return this;
+    }
+
+    public PayPalWidget actionContinuePay() {
+        waitForElementClickable(this.continuePayButton);
+        driver.findElement(this.continuePayButton).click();
+        waitForElementHidden(this.spinner);
+        return this;
+    }
+
+    public PayPalWidget setEmail(String email) {
+        WebElement element = driver.findElement(this.email);
+        element.clear();
+        element.sendKeys(email);
+        return this;
+    }
+
+    public PayPalWidget setPassword(String password) {
+        WebElement element = driver.findElement(this.password);
+        element.clear();
+        element.sendKeys(password);
+        return this;
+    }
+
+    public PayPalWidget switchIFrame() {
+        driver.switchTo().frame("injectedUl");
+        return this;
+    }
+
+    public PayPalWidget switchIFrameToDefault() {
+        driver.switchTo().defaultContent();
+        return this;
+    }
+
+}
