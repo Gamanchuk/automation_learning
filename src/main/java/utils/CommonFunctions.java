06b4d87b03a25aa572103548d1b99bd4bfd6c198
eugene <shapovalovei@gmail.com>
2017-02-09 18:28:01 +0200

    add import org.testng.Reporter;

--- src/main/java/utils/CommonFunctions.java
+++ src/main/java/utils/CommonFunctions.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
 import org.testng.Reporter;
 import ru.yandex.qatools.allure.annotations.Attachment;
 
