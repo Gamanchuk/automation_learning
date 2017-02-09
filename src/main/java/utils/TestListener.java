44b67301eb550546c940be522f3929d3da2aa810
eugene <shapovalovei@gmail.com>
2017-02-09 18:19:59 +0200

    Add TestListener Add TestRail Start Run

---
+++ src/main/java/utils/TestListener.java
@@ -0,0 +1,62 @@
+package utils;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class TestListener implements ITestListener {
+    private static Log log = LogFactory.getLog(TestListener.class);
+
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+    }
+
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        if (Boolean.valueOf(System.getProperty("projectTracking"))) {
+
+            String runName = "Run on " + System.getProperty("device.name") + " - " + new Date().toString();
+            try {
+                TestRailRunHelper.getInstance().startRun(runName);
+                log.info("Create Run in TestRail with name: [" + runName +"]");
+            } catch (IOException e) {
+                e.printStackTrace();
+                log.error("Cannot create TestRail Run [" + runName + "]: " + e.getLocalizedMessage());
+            }
+        }
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+    }
+}
