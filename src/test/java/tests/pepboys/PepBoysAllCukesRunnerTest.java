b49ff2827107686911951c2239ba1d0615741c0b
eugene <shapovalovei@gmail.com>
2017-02-10 16:23:13 +0200

    Delete old imports

--- src/test/java/tests/pepboys/PepBoysAllCukesRunnerTest.java
+++ src/test/java/tests/pepboys/PepBoysAllCukesRunnerTest.java
@@ -1,20 +1,12 @@
 package tests.pepboys;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-import org.junit.runner.RunWith;
-import org.testng.annotations.Listeners;
-import org.testng.annotations.Test;
-import utils.AllureListener;
-import utils.TestListener;
 
 
 @CucumberOptions (
         features = "src/test/resources/feature/pepboys/",
         glue = "steps")
-
-
 class PepBoysAllCukesRunnerTest extends AbstractTestNGCucumberTests {
 
 }
