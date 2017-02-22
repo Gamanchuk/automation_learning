ea2703e988fb0e57003c9c7be30e42b8842cbe56
eugene <shapovalovei@gmail.com>
2017-02-23 00:57:51 +0200

    Set up retries

---
+++ src/main/java/utils/retries/RetryException.java
@@ -0,0 +1,21 @@
+package utils.retries;
+
+import org.testng.SkipException;
+
+import java.io.Serializable;
+
+/**
+ * Extends <code>SkipException</code> for retry.
+ */
+public class RetryException extends SkipException implements Serializable {
+    private static final long serialVersionUID = 6543627546732546750L;
+
+    public RetryException(String message) {
+        super(message);
+    }
+
+    public RetryException(String message, Throwable cause) {
+        super(message, cause);
+        reduceStackTrace();
+    }
+}
