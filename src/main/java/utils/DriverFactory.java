3571d6da1dbd155676354b929f62d787679e751e
eugene <shapovalovei@gmail.com>
2017-03-26 14:14:20 +0300

    Capability 'realDeviceLogger' deprecated. Delete it from framework

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -81,13 +81,12 @@ public class DriverFactory {
                         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
                         desiredCapabilities.setCapability("wdaLocalPort", Integer.parseInt(iproxyPort));
                         desiredCapabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT, 500000);
-                        // desiredCapabilities.setCapability("realDeviceLogger", "/usr/local/lib/node_modules/deviceconsole/deviceconsole");
                         desiredCapabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, true);
                         desiredCapabilities.setCapability("useNewWDA", true);
                         desiredCapabilities.setCapability("safariAllowPopups", true);
                         // desiredCapabilities.setCapability("xcodeOrgId", "Y95G5M3Q84");
                         // desiredCapabilities.setCapability("xcodeSigningId", "iPhone Developer");
-                        // desiredCapabilities.setCapability("realDeviceLogger", "/usr/local/lib/node_modules/deviceconsole/deviceconsole");
+                        
                     }
 
                     eventListener = new MyWebDriverEventListener();
