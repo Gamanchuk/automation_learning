6890a416c60ef6ca77680878f3a423a6c12abb45
eugene <shapovalovei@gmail.com>
2017-04-03 12:09:46 +0300

    AUTO-88: Proceed to guest checkout

--- src/main/java/entities/components/SignInFormComponent.java
+++ src/main/java/entities/components/SignInFormComponent.java
@@ -5,9 +5,8 @@ import org.openqa.selenium.WebElement;
 import utils.CommonFunctions;
 
 public class SignInFormComponent extends BaseComponent {
-    private String submitButton = "//div[contains(@class,'submit-button')]";
-    private By signInButton = By.xpath(submitButton + "//button");
-    private By forgotPassword = By.xpath(submitButton + "//a");
+    private By signInButton = By.xpath("//div[contains(@class,'submit-button')]//button");
+    private By forgotPassword = By.xpath("//div[contains(@class,'login-buttons')]//a");
     private By emailField = By.id("shipping-email");
     private By passwordField = By.id("password");
 
