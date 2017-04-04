1be92a5e57ccdc79648a3e07347258ba439fef29
eugene <shapovalovei@gmail.com>
2017-04-04 21:42:24 +0300

    Update component action select()

--- src/main/java/entities/components/RadioListComponent.java
+++ src/main/java/entities/components/RadioListComponent.java
@@ -1,15 +1,38 @@
 package entities.components;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.CommonFunctions;
+
+import java.util.List;
 
 public class RadioListComponent extends BaseComponent {
 
     public void select(String option) {
         String selectedOptionPath = "//div[contains(@class, 'radio-list-option-selected')]";
+        String itemPath = "//div[contains(@class, 'radio-list-option')]";
+        String itemDetailsPath = "//div[contains(@class, 'radio-list-details')]";
+        String itemListPath = "//div[contains(@class, 'radio-expanded')]";
         waitForElementVisible(By.xpath(selectedOptionPath));
-        if(!isElementPresent(By.xpath(selectedOptionPath + "//div[contains(text(), '" + option + "')]"))) {
-            click(By.xpath("//div[contains(@class, 'radio-list') and contains(@class, 'radio-collapsed')]"));
-            click(By.xpath("(//div[contains(text(), '" + option + "')])[1]"));
+
+        String actualItem = getDriver()
+                .findElement(By.xpath(selectedOptionPath + itemDetailsPath + "//div"))
+                .getText();
+
+        if (!actualItem.contains(option)) {
+            getDriver().findElement(By.xpath(selectedOptionPath)).click();
+            CommonFunctions.attachScreenshot("List methods");
+
+            List<WebElement> listWebElement = getDriver()
+                    .findElements(By.xpath(itemListPath + itemPath + itemDetailsPath + "//div"));
+
+            for (WebElement element : listWebElement) {
+                if (element.getText().contains(option)) {
+                    log.info("Method contains: " + element.getText());
+                    element.click();
+                    break;
+                }
+            }
         }
     }
 }
