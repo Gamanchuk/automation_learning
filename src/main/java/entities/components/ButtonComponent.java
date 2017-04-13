969a10d18b9efdc409e88e84c5d9989b970d5944
eugene <shapovalovei@gmail.com>
2017-04-13 12:38:56 +0300

    add sleep before click button

--- src/main/java/entities/components/ButtonComponent.java
+++ src/main/java/entities/components/ButtonComponent.java
@@ -1,14 +1,18 @@
 package entities.components;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class ButtonComponent extends BaseComponent {
     private By btn = By.xpath("//div[contains(@class, 'submit-button')]//button");
 
     public void clickButton() {
-        WebElement element = getDriver().findElements(btn).get(0);
-        element.click();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getDriver().findElements(btn).get(0).click();
+
     }
 
     public boolean isOverlayDisplayed() {
