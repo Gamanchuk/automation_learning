627cf13e0ed9053bce9f004deb0bb118a7268122
eugene <shapovalovei@gmail.com>
2017-04-13 13:55:10 +0300

    change click to doubleClick

--- src/main/java/entities/components/ButtonComponent.java
+++ src/main/java/entities/components/ButtonComponent.java
@@ -1,13 +1,17 @@
 package entities.components;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class ButtonComponent extends BaseComponent {
     private By btn = By.xpath("//div[contains(@class, 'submit-button')]//button");
 
     public void clickButton() {
-        getDriver().findElement(btn).click();
-
+        WebElement element = getDriver().findElement(btn);
+        Actions actions = new Actions(getDriver());
+        actions.doubleClick(element);
+        actions.perform();
     }
 
     public boolean isOverlayDisplayed() {
