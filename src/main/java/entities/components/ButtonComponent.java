183b134f6a37b0536e0c4ac7db9d7a88e412d76a
eugene <shapovalovei@gmail.com>
2017-04-13 12:53:21 +0300

    change find func

--- src/main/java/entities/components/ButtonComponent.java
+++ src/main/java/entities/components/ButtonComponent.java
@@ -6,12 +6,7 @@ public class ButtonComponent extends BaseComponent {
     private By btn = By.xpath("//div[contains(@class, 'submit-button')]//button");
 
     public void clickButton() {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        getDriver().findElements(btn).get(0).click();
+        getDriver().findElement(btn).click();
 
     }
 
