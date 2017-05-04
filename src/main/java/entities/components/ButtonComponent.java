5ead21d4f35fbb6c401cd5893c53ee03d4e56996
eugene <shapovalovei@gmail.com>
2017-05-04 17:26:36 +0300

    WiP commit: change click strategy

--- src/main/java/entities/components/ButtonComponent.java
+++ src/main/java/entities/components/ButtonComponent.java
@@ -1,12 +1,14 @@
 package entities.components;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 public class ButtonComponent extends BaseComponent {
     private By btn = By.xpath("//div[contains(@class, 'submit-button')]//button");
 
     public void clickButton() {
-        getDriver().findElements(btn).get(0).click();
+        //getDriver().findElements(btn).get(0).click();
+        getDriver().findElements(btn).get(0).sendKeys(Keys.RETURN);
     }
 
 
