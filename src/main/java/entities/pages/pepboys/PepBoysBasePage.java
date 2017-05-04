406adbe96dd98ba6660f042a1fde6f044a0517b2
Eugene Shapovalov <shapovalovei@gmail.com>
2017-05-04 09:07:26 +0300

    Hotfix: updated cookies for making appointment

--- src/main/java/entities/pages/pepboys/PepBoysBasePage.java
+++ src/main/java/entities/pages/pepboys/PepBoysBasePage.java
@@ -4,5 +4,5 @@ import entities.Entity;
 
 public abstract class PepBoysBasePage extends Entity {
     public final String BASE_URL = "https://mstage.stage.pepboys.com/";
-    public final String COOKIES = "?_mwexperienceid=75b123b0-6444-4893-9825-1303473bc59f";
+    public final String COOKIES = "?var=b&utm_expid=95483662-2.I0S4ga4uSlCoEu3KU2QnNw.1&?_mwexperienceid=75b123b0-6444-4893-9825-1303473bc59f";
 }
