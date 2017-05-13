ec21b3799f8324081b9f4ef6f5d39585661cdb36
eugene <shapovalovei@gmail.com>
2017-05-13 22:26:21 +0300

    WiP commit for AUTO-370: update full and debug suites

--- src/test/java/tests/pepboys/PepBoysDebugCukesRunnerTest.java
+++ src/test/java/tests/pepboys/PepBoysDebugCukesRunnerTest.java
@@ -3,12 +3,11 @@ package tests.pepboys;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-
-@CucumberOptions (
+@CucumberOptions(
         features = "src/test/resources/features/pepboys/",
-        glue = "steps.pepboys",
-        tags = "@smoke"
+        glue = "steps",
+        tags = "@debug"
 )
-class PepBoysSmokeCukesRunnerTest extends AbstractTestNGCucumberTests {
+class PepBoysDebugCukesRunnerTest extends AbstractTestNGCucumberTests {
 
 }
