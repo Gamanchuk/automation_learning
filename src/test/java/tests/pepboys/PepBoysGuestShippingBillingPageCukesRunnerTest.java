19c0908dc063586de9e5628e5447053846d5e3fa
eugene <shapovalovei@gmail.com>
2017-05-13 20:48:45 +0300

    WiP commit: feature for qvc

--- src/test/java/tests/pepboys/PepBoysGuestShippingBillingPageCukesRunnerTest.java
+++ src/test/java/tests/pepboys/PepBoysGuestShippingBillingPageCukesRunnerTest.java
@@ -5,7 +5,7 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 
 @CucumberOptions (
-        features = "src/test/resources/features/pepboys/",
+        features = "src/test/resources/features/pepboys/guest_shipping_and_billing_page/",
         glue = "steps.pepboys",
         tags = "@regression"
 )
