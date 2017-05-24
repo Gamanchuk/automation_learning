d67a887f2a85faeee2bea4920faf11902d80ab86
eugene <shapovalovei@gmail.com>
2017-05-24 14:59:49 +0300

    WiP commit.

--- src/main/java/entities/components/CollapserComponent.java
+++ src/main/java/entities/components/CollapserComponent.java
@@ -9,11 +9,17 @@ public class CollapserComponent extends BaseComponent {
     // TODO: update this method to use collapser name
     public void openCollapser() {
 
-        WebElement collapser = findElements(By.cssSelector("a.collapser-link")).get(0);
+        WebElement collapser = findElement(By.cssSelector("a.collapser-link"));
 
         if (collapser.getAttribute("class").contains("collapsed")) {
             javascriptScroll(collapser);
             collapser.click();
+
+            WebElement collapser2 = findElement(By.cssSelector("a.collapser-link"));
+            if (collapser.getAttribute("class").contains("collapsed")) {
+                collapser2.click();
+            }
+
             //click(By.cssSelector("a.collapser-link"));
         }
     }
