99c6986a47271d5c069a163d975231af7bd3df6d
eugene <shapovalovei@gmail.com>
2017-06-29 19:05:35 +0300

    AUTO-675 - add param

--- src/main/java/entities/pages/qvc/QVCProductPage.java
+++ src/main/java/entities/pages/qvc/QVCProductPage.java
@@ -81,7 +81,8 @@ public class QVCProductPage extends BasePage {
     }
 
     public void setCookies() {
-        log.info("PR NUMBER: " + System.getProperty("pr.number"));
+        log.info("PR NUMBER: " + System.getProperty("mw.pr.number"));
+        log.info("PR LOADER: " + System.getProperty("mw.pr.loader"));
         getDriver().get(BASE_URL + COOKIES);
     }
 }
