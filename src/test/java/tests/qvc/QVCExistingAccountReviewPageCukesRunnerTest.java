221971d7e4a02e28623aa58c90e63f4407f728fb
Евгений Шаповалов <EIShapovalov@alfabank.ru>
2017-07-06 12:12:24 +0300

    change featere location

--- src/test/java/tests/qvc/QVCExistingAccountReviewPageCukesRunnerTest.java
+++ src/test/java/tests/qvc/QVCExistingAccountReviewPageCukesRunnerTest.java
@@ -4,7 +4,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        features = "src/test/resources/features/qvc/existing_account_review_page/",
+        features = "src/test/resources/features/qvc/existing_account_payment_page/",
         glue = "steps",
         tags = {"@qvc", "~@Ignored"}
 )
