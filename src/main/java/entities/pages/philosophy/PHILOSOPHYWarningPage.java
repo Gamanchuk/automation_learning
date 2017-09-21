705426d6bb56d6c66c8fb771e76ec015edbfab64
Eugene Shapovalov <shapovalovei@gmail.com>
2017-09-21 14:32:47 +0300

    discard warning checks on philosophy production

--- src/main/java/entities/pages/philosophy/PHILOSOPHYWarningPage.java
+++ src/main/java/entities/pages/philosophy/PHILOSOPHYWarningPage.java
@@ -13,23 +13,23 @@ public class PHILOSOPHYWarningPage extends BasePage {
 
     @Override
     public boolean isPage() {
-        if (Config.SITE_NAME.equals("philosophy-prod")) {
-            return true;
-        } else {
+        if (Config.SITE_NAME.equals("philosophy-stage")) {
             return isElementVisible(warningOldIOS, 20) || isElementVisible(warningNewIOS, 5);
+        } else {
+            return true;
         }
     }
 
     public boolean isPage(int timeout) {
-        if (Config.SITE_NAME.equals("philosophy-prod")) {
-            return true;
-        } else {
+        if (Config.SITE_NAME.equals("philosophy-stage")) {
             return isElementPresent(warningOldIOS, timeout) || isElementPresent(warningNewIOS, 5);
+        } else {
+            return true;
         }
     }
 
     public void ignoreWarning() {
-        if (Config.SITE_NAME.equals("philosophy-prod")) {
+        if (Config.SITE_NAME.equals("philosophy-stage")) {
             CommonFunctions.executeJavaScript("PhishingAlertController.ignoreWarningSelected();");
         }
     }
